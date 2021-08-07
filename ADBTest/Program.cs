274fb1bbf82c91713da591768285cd14224d5ff3
Baha2000 <dragon.baha@yandex.ru>
2021-08-07 17:58:30 +0300

    Init Commit

---
+++ ADBTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using SharpAdbClient;
+using SharpAdbClient.DeviceCommands;
+
+namespace ADBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdbServer server = new();
+            var result = server.StartServer(@"C:\Info\Android\platform-tools\adb.exe", restartServerIfNewer: false);
+            
+            AdbClient client = new();
+            var devices = client.GetDevices();
+
+            foreach (var device in devices)
+            {
+                Console.WriteLine(device);
+            }
+
+            PackageManager manager = new PackageManager(client, devices[0]);
+
+            manager.InstallPackage(@"C:\Info\Android\com.finchtechnologies.trackingtestingandroid.apk", reinstall: true);
+
+            //команда для передачи на пуш файл adb shell pm grant com.finchtechnologies.trackingtestingandroid android.permission.WRITE_EXTERNAL_STORAGE
+            var receiver = new ConsoleOutputReceiver();
+            //client.ExecuteRemoteCommand("adb shell", devices[0], receiver);
+            //Console.WriteLine(receiver.ToString());
+            //client.ExecuteRemoteCommand("su", devices[0], receiver);
+            //Console.WriteLine(receiver.ToString());
+            client.ExecuteRemoteCommand("mount -o remount,rw /system",  devices[0], receiver);
+            Console.WriteLine(receiver.ToString());
+            Console.ReadKey();
+
+            using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), devices[0]))
+            using (Stream stream = File.OpenRead(@"C:\Info\config"))
+            {
+                service.Push(stream, "/Databases/Standard/", 444, DateTime.Now, null, CancellationToken.None);
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
